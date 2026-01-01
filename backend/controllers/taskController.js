5bc7590f59cddec0d819c0fdb1f3c0c75a44607e
lmnst <lmnstzz@gmail.com>
2026-01-02 04:58:47 +0800

    Create and Read MVP

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,11 +1,11 @@
-import Task from '../models/taskModel';
+import Task from '../models/taskModel.js';
 
 
 // @desc    Retrieve all tasks for the current user
 // @route   GET /api/tasks
 // @access  Private (Requires login)
-const getTasks = async (req, res) => {
-    const tasks = await Task.find({ user: req.user,id });
+export const getTasks = async (req, res) => {
+    const tasks = await Task.find({ user: req.user._id });
 
     res.status(200).json(tasks);
 }
@@ -13,8 +13,8 @@ const getTasks = async (req, res) => {
 //@desc Create new task
 //@route POST/api/tasks
 //@access Private
-const createTask = async (req, res) => {
-    if (!req.body.tittle) {
+export const createTask = async (req, res) => {
+    if (!req.body.title) {
         res.status(400);
         throw new Error('Please add task title');
     }
@@ -22,10 +22,9 @@ const createTask = async (req, res) => {
     const task = await Task.create({
         title: req.body.title,
         description: req.body.description,
-        user: req.user.id,
+        user: req.user._id,
     });
 
     res. status(200).json(task);
 }
 
-module.exports = {getTasks, createTask,};
