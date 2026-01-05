2c83a6fbca05c0e34482850b0eccb1a72382bd0d
lmnst <lmnstzz@gmail.com>
2026-01-06 03:13:57 +0800

    删掉无用引用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,10 @@
-// // src/App.tsx
-
-// import { Routes, Route, Navigate } from 'react-router-dom'; 
-
-// import LoginPage from './pages/LoginPage.tsx';
-// import RegisterPage from './pages/RegisterPage.tsx';
-// import Navbar from './components/Navbar.tsx';
-// import TasksPage from './pages/TasksPage.tsx'; // 1. 引入 TasksPage
-
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <main>
-//         <Routes>
-//           <Route path="/login" element={<LoginPage />} />
-//           <Route path="/register" element={<RegisterPage />} />
-//           <Route path="/tasks" element={<TasksPage />} /> {/* 2. 添加新路由 */}
-//           <Route path="/" element={<Navigate to="/login" />} />
-//         </Routes>
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import {BrowserRouter as Routes, Route, Navigate} from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage.tsx';
 import RegisterPage from './pages/RegisterPage.tsx';
 import Navbar from './components/Navbar.tsx';
 import TasksPage from './pages/TasksPage.tsx';
-import ProtectedRouter from './components/ProtextedRoutr.tsx';
+import ProtectedRouter from './components/ProtectedRoutr.tsx';
 import './App.css';
 
 function App() {
