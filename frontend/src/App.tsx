8b5c69e7afb710d1072a4a46d5bc9d0202ad8ea4
lmnst <lmnstzz@gmail.com>
2026-01-06 03:55:53 +0800

    修改了已知错误

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom'; 
 import LoginPage from './pages/LoginPage.tsx';
 import RegisterPage from './pages/RegisterPage.tsx';
 import Navbar from './components/Navbar.tsx';
