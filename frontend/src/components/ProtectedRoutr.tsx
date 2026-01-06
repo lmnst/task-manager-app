5bf8180d959d85de1212c2b5ca4e26747377faa6
lmnst <lmnstzz@gmail.com>
2026-01-06 20:49:59 +0800

    解决 React 状态持久化 与 受控路由同步 的同步

--- frontend/src/components/ProtectedRoutr.tsx
+++ frontend/src/components/ProtectedRoutr.tsx
@@ -3,7 +3,11 @@ import{ Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const ProtectedRouter = () => {
-    const { userInfo } = useAuth();
+    const { userInfo, loading } = useAuth() as any;
+
+    if(loading) {
+        return <div>Checking auth status...</div>
+    }
 
     return userInfo ? <Outlet/> : <Navigate to="/login" replace />;
 };
